4732bd3f44b84daeb504c584e8a9daecf684c173
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-09-25 11:06:29 +0000

    completo

--- src/app.js
+++ src/app.js
@@ -8,4 +8,31 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let act1 = excuseGenerate();
+  console.log(act1);
+  document.querySelector("#excuse").innerHTML = act1;
 };
+function excuseGenerate() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  return (
+    randomElement(who) +
+    " " +
+    randomElement(action) +
+    " " +
+    randomElement(what) +
+    " " +
+    randomElement(when)
+  );
+}
+function randomElement(Array) {
+  return Array[Math.floor(Math.random() * Array.length)];
+}
